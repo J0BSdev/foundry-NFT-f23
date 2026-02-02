a779225d1d1d051064d4d1e9fa8906b42ce33f6c
Lovro <lovro.posel79@gmail.com>
2026-02-02 20:43:04 +0100

    update

--- script/Interaction.s.sol
+++ script/Interaction.s.sol
@@ -3,8 +3,28 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
-contract Interaction is Script {
+
+
+
+contract MintBasicNFT is Script{
+string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+
+address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNFT", block.chainid);
+
+mintNftOnContract(mostRecentlyDeployed);
+
+}
+
+function mintNftOnContract(address contractAddress) public {
+    vm.startBroadcast();
+    BasicNFT(contractAddress).mintNFT(PUG);
+    vm.stopBroadcast();
+
     
     }
 }
